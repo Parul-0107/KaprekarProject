fe34cf6355b7a4691489114e0afd436938810cac
Parul-0107 <parulaggarwal1112@gmail.com>
2019-12-10 19:51:39 -0600

    The code uses the correct command line arguments as required
    Have changed the code that uses the correct command line arguments

--- c_version/kaprekarDemo.c
+++ c_version/kaprekarDemo.c
@@ -6,11 +6,11 @@
 int main(int argc, char **argv) {
 
   if(argc != 2) {
-    fprintf(stderr, "Usage: %s n\n", argv[0]);
+    fprintf(stderr, "Usage: %s n\n", argv[1]);
     exit(1);
   }
 
-  int n = atoi(argv[1]);
+  int n = atoi(argv[2]);
 
   if(isKaprekar(n)) {
     printf("%d is a Kaprekar Number!\n", n);
